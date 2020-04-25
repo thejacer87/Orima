ea61543a32684a26abf931c5808d2cf034dcb1ab
Jace Bennest <jacebennest87@gmail.com>
2020-04-25 00:06:23 -0700

    fix up gameover transition

--- scripts/GameOver.gd
+++ scripts/GameOver.gd
@@ -3,4 +3,4 @@ extends Control
 
 func _ready() -> void:
 	yield(get_tree().create_timer(6.0), "timeout")
-	SceneTransition.change_scene(Globals.scenes["menu"], Vector2.ZERO)
+	get_tree().change_scene(Globals.scenes["menu"])
