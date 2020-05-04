120d3a8326859efbcfebafaef2b54f1b5cae0eb0
Jace Bennest <jacebennest87@gmail.com>
2020-05-03 23:54:58 -0700

    piranha plant done

--- scripts/Enemies/PiranhaPlant.gd
+++ scripts/Enemies/PiranhaPlant.gd
@@ -1,10 +1,42 @@
 extends KinematicBody2D
 
-onready var animation_player = $AnimationPlayer
+const ANIM_HEIGHT := 24
+
+var pause := false
+onready var tween_up = $TweenUp
+onready var tween_down = $TweenDown
+
+func _ready() -> void:
+	pipe_up()
 
 
+func _on_DetectionZone_body_entered(body: Node) -> void:
+	pause = true
 
 
-func _on_Timer_timeout():
-	print("timeout")
-	animation_player.play("exit_pipe")
+func _on_DetectionZone_body_exited(body: Node) -> void:
+	pause = false
+	pipe_up()
+
+
+func _on_TweenUp_tween_completed(object: Object, key: NodePath) -> void:
+	yield(get_tree().create_timer(1.0), "timeout")
+	pipe_down()
+
+
+func _on_TweenDown_tween_completed(object: Object, key: NodePath) -> void:
+	yield(get_tree().create_timer(1.0), "timeout")
+	pipe_up()
+
+
+func pipe_up():
+	if not pause:
+		tween_up.interpolate_property(self, "position",
+			position, Vector2(position.x, position.y - ANIM_HEIGHT), 0.75)
+		tween_up.start()
+
+
+func pipe_down():
+	tween_down.interpolate_property(self, "position",
+		position, Vector2(position.x, position.y + ANIM_HEIGHT), 0.75)
+	tween_down.start()
--- scripts/Level_1-2.gd
+++ scripts/Level_1-2.gd
@@ -9,6 +9,7 @@ func _ready() -> void:
 		if cell_id != dynamic_tilemap.INVALID_CELL:
 			var tile_name = dynamic_tilemap.tile_set.tile_get_name(cell_id)
 			var child
+			var offset = Vector2.ZERO
 			match tile_name:
 				"brick.png 0":
 					child = Globals.BRICK.instance()
@@ -25,8 +26,12 @@ func _ready() -> void:
 					child.get_node("Items").add_child(mushroom)
 				"piranha_open.png 4":
 					child = Globals.Enemies.PIRANHA.instance()
+					offset = Vector2(8, 0)
+					print(child.position)
 
 			if child != null:
-				child.position = dynamic_tilemap.map_to_world(cell) + (dynamic_tilemap.cell_size / 2)
+				child.position = dynamic_tilemap.map_to_world(cell) + (dynamic_tilemap.cell_size / 2) + offset
+				if tile_name == "piranha_open.png 4":
+					print(child.position)
 				self.add_child(child)
 	dynamic_tilemap.clear()
