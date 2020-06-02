154ab61f0b698a14f81d78f4036c1a81272b067a
Jace Bennest <jacebennest87@gmail.com>
2020-06-01 18:40:27 -0700

    bowser fireball started

--- scripts/Enemies/Bowser.gd
+++ scripts/Enemies/Bowser.gd
@@ -1,5 +1,15 @@
 extends Node2D
 
+onready var animation_player := $AnimationPlayer
+
 
 func _ready() -> void:
 	pass
+
+
+func _on_Body_body_entered(player: Player) -> void:
+	player.damage()
+
+
+func _on_Timer_timeout() -> void:
+	animation_player.play("jump")
