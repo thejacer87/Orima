2c886d1a105e52be90d463d3fedd82936a317f1d
Jace Bennest <jacebennest87@gmail.com>
2020-06-01 19:13:07 -0700

    fireball done

--- scripts/Enemies/Fireball.gd
+++ scripts/Enemies/Fireball.gd
@@ -1,5 +1,18 @@
-extends Area2D
+extends StaticBody2D
 
+const SPEED := -50
+
+onready var animation_player := $AnimationPlayer
+
+func _ready() -> void:
+	animation_player.play("move")
+	
+func _physics_process(delta) -> void:
+	position.x += SPEED * delta
 
 func _on_Fireball_body_entered(player: Player) -> void:
 	player.damage()
+
+
+func _on_VisibilityNotifier2D_screen_exited():
+	queue_free()
