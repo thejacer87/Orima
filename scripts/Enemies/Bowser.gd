d06e271fa66a76160ed74f0e15204af65ff1231f
Jace Bennest <jacebennest87@gmail.com>
2020-06-02 19:22:29 -0700

    decent start i guess

--- scripts/Enemies/Bowser.gd
+++ scripts/Enemies/Bowser.gd
@@ -1,4 +1,4 @@
-extends Node2D
+extends KinematicBody2D
 
 const LEFT_LIMIT := -72
 const RIGHT_LIMIT := 32
@@ -8,18 +8,19 @@ onready var mouth := $Body/Mouth
 onready var fireball := preload("res://scenes/Enemies/Fireball.tscn")
 
 var original_position
+var velocity := Vector2()
 
 func _ready() -> void:
 	original_position = position
 
+func _physics_process(delta) -> void:
+	move_toward_orima(delta)
+	velocity.y += 50 * delta
+	move_and_slide(velocity, Vector2.UP)
+	
 
 func _on_Body_body_entered(player: Player) -> void:
 	player.damage()
-
-
-func _on_Timer_timeout() -> void:
-	animation_player.play("jump")
-	move_toward_orima()
 
 
 func _on_FireTimer_random_timeout() -> void:
@@ -28,6 +29,15 @@ func _on_FireTimer_random_timeout() -> void:
 	get_tree().get_root().add_child(fb_instance)
 
 
-func move_toward_orima() -> void:
-	var dest = lerp(position, original_position + Vector2(LEFT_LIMIT, 0), .5)
-	position = dest
+func jump() -> void:
+	if is_on_floor():
+		print("jump")
+		velocity.y -= 250
+	
+func move_toward_orima(delta) -> void:
+	var dest = lerp(position, original_position + Vector2(LEFT_LIMIT, 0), .5) * delta
+	position.x = dest.x
+
+
+func _on_JumpTimer_random_timeout():
+	jump()
